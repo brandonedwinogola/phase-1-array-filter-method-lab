7055e87af93e4ebf8b7a2dd9018dcf97d7c830b1
bran don <106029484+brandonedwinogola@users.noreply.github.com>
2022-06-12 11:34:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,64 @@
 // Code your solution here
+const "drivers" = [{
+    ("Bobby", "Sammy", "Sally", "Annette", "Sarah", "bobby")
+
+}]
+console.log(drivers.names) = Matchingdrivers;
+
+drivers(Matchingdrivers);
+// driver "Bobby"= ("Bobby", "bobby")
+// driver "Sammy" = ("Sammy")
+// driver "Sally" = ("Sally")
+// driver "Annette" = ("Annette")
+// driver "Sarah" = ("Sarah")
+
+
+const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "bobby"];
+
+function() {
+    drivers.length = 0
+
+    drivers.push('Bobby', 'Sammy', 'sally', 'Annette', 'sarah', 'bobby');
+};
+
+console.log(drivers.names) {
+    return drivers.names === "sa";
+};
+// LOG: Sarah
+// LOG: Sammy
+// LOG: Sally
+
+const drivers = [{
+        name: "Bobby",
+        homeTown: "Pittsburgh",
+    },
+    {
+        name: 'Sammy',
+        homeTown: 'New York',
+    },
+    {
+        name: 'Sally',
+        homeTown: 'Cleveland',
+    },
+    {
+        name: 'Annette',
+        homeTown: 'Los Angeles',
+    },
+    {
+        name: 'Bobby',
+        homeTown: 'Tampa Bay',
+    },
+];
+
+function filter(homeTown) {
+    console.log(drivers $ { homeTown }!);
+}
+
+drivers, 'Bobby';
+// LOG: {
+name: "Bobby";
+homeTown: "Pittsburgh",
+}; {
+    name: "Bobby",
+    homeTown: "Tampa Bay",
+};
